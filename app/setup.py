c9dba6f23b04197d36f0a7f7ef64b2a9565d3592
hari-testpress <hari@testpress.in>
2022-12-07 11:16:23 +0530

    Create subproject

--- app/setup.py
+++ app/setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
-
 requirements = ["ipython>=6", "requests>=2"]
 
 setup(
@@ -11,7 +8,7 @@ setup(
     author="hari",
     author_email="hari@testpress.in",
     description="A package",
-    long_description=readme,
+    long_description="readme",
     long_description_content_type="text/markdown",
     url="https://github.com/testpress/testpress_python/",
     packages=find_packages(),
